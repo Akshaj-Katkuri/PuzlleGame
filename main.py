65a1bba0f9f21b133e8062648e800bf6b0d7668a
Akshaj Katkuri <akshaj2312@gmail.com>
2025-02-11 13:42:56 -0500

    finished level 6

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Surface
 import sys
+import random
 
 
 # Initialize Pygame
@@ -33,7 +34,8 @@ lvl5_image = pygame.image.load("level_5.png")
 lvl5_image = pygame.transform.scale(lvl5_image, (LEVEL_5_IMAGE_WIDTH, LEVEL_5_IMAGE_HEIGHT))
 
 # User info
-levels_unlocked = [1]
+levels_unlocked = [1,2,3,4,5]
+type_duration = 0
 
 # screen info
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -115,18 +117,19 @@ def level_1():
             else: 
                 popup(wrong_image, 1000)
 
-def handle_user_input(clue_text: list[str], width: float, image=None, clue_box_y: float=(0.5*HEIGHT), image_width: float=None, image_height: float=None) -> str:
-    global user_input
+def handle_user_input(clue_text: list[str], width: float, image=None, clue_box_y: float=(0.5*HEIGHT), image_width: float=None, image_height: float=None, font_size: int = 36, time_limit: int=None) -> str:
+    global user_input, type_duration
+    start_time = None
     user_input = ""
     input_box_y: float = clue_box_y  # Static y-coordinate proportional to screen height
     input_box = pygame.Rect(WIDTH//2 - (0.5 * width), input_box_y, width, 40)
-    clue_font = pygame.font.Font(None, 36)
+    clue_font = pygame.font.Font(None, font_size)
 
     while True:
         screen.fill(BG_COLOR)
         
         # Display the clue text
-        display_clue(clue_text)
+        display_clue(clue_text, font_size)
         
         # Draw the input box and user input
         pygame.draw.rect(screen, LEVEL_BUTTON_COLOR, input_box, 2)
@@ -142,21 +145,30 @@ def handle_user_input(clue_text: list[str], width: float, image=None, clue_box_y
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
+                if time_limit and user_input == "":
+                    start_time = pygame.time.get_ticks()
                 if event.key == pygame.K_ESCAPE:
                     set_screen_state('main_menu')
                     return ""
                 elif event.key == pygame.K_RETURN:
+                    if time_limit:
+                        type_duration = pygame.time.get_ticks() - start_time
                     return user_input
                 elif event.key == pygame.K_BACKSPACE:
                     user_input = user_input[:-1]
                 else:
                     if width == SMALL_CLUE_BOX_WIDTH and len(user_input) < 9:
                         user_input += event.unicode
-                    elif width == BIG_CLUE_BOX_WIDTH and len(user_input) < 46:
+                    elif width == BIG_CLUE_BOX_WIDTH and len(user_input) < 70:
                         user_input += event.unicode
 
-def display_clue(clue_text: list[str]):
-    clue_font = pygame.font.Font(None, 36)
+        if time_limit and start_time: 
+            type_duration = pygame.time.get_ticks() - start_time
+            if type_duration > time_limit:
+                return user_input
+
+def display_clue(clue_text: list[str], font_size: int = 36):
+    clue_font = pygame.font.Font(None, font_size)
     for i, line in enumerate(clue_text):
         text_surface = clue_font.render(line, True, TEXT_COLOR)
         screen.blit(text_surface, (WIDTH//2 - text_surface.get_width()//2, HEIGHT//6 + i * 40))
@@ -178,7 +190,7 @@ def level_2():
         "What word am I?"
     ]
 
-    user_input = handle_user_input(clue_text, SMALL_CLUE_BOX_WIDTH, None, (0.5*HEIGHT))
+    user_input = handle_user_input(clue_text, SMALL_CLUE_BOX_WIDTH)
     if user_input.upper() == "EMPTY":
         unlock_level(3)
         set_screen_state('main_menu')
@@ -231,22 +243,37 @@ def level_4():
         popup(wrong_image, 1000)
 
 def level_5():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                set_screen_state('main_menu')
-    
-    user_input = handle_user_input(["What is this color according to the gray scale image?"], SMALL_CLUE_BOX_WIDTH, lvl5_image, 0.9*HEIGHT, LEVEL_5_IMAGE_WIDTH, LEVEL_5_IMAGE_HEIGHT)
+    user_input = handle_user_input([""], 
+                                   SMALL_CLUE_BOX_WIDTH, 
+                                   lvl5_image, 
+                                   0.9*HEIGHT, 
+                                   LEVEL_5_IMAGE_WIDTH, 
+                                   LEVEL_5_IMAGE_HEIGHT)
     if user_input.upper() == "RED":
         popup(right_image, 1000)
         unlock_level(6)
         set_screen_state('main_menu')
     else:
         popup(wrong_image, 1000)
+
+def level_6():
+    random_words = get_random_words('words.txt', 5)
+    user_input = handle_user_input(["Typing Test!", "Type the words as fast as you can (full accuracy)", random_words], BIG_CLUE_BOX_WIDTH, font_size=24, time_limit=10000)
+    if user_input.upper() == random_words.upper() and type_duration <= 10000:
+        popup(right_image, 1000)
+        unlock_level(7)
+        set_screen_state('main_menu')
+    else:
+        popup(wrong_image, 1000)
+
+def level_7():
+    screen.fill(BG_COLOR)
+    clue_text = ['Solve this integral!']
+
+def get_random_words(file_path, num_words):
+    with open(file_path, 'r') as file:
+        words = file.read().splitlines()
+    return ' '.join(random.sample(words, num_words))
 
 def popup(img: Surface, duration):
     start_time = pygame.time.get_ticks()
@@ -285,5 +312,11 @@ while True:
 
         case 'level_5':
             level_5()
+
+        case 'level_6':
+            level_6()
+
+        case 'level_7':
+            level_7()
 
     pygame.display.update()
